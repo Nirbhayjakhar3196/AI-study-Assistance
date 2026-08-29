29d23a0a49bc80ccc7f7c98cd3ddcc7a69cf3ae9
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-08-29 22:07:14 +0530

    feat:Added embedding

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { extractPdfText } from "@/lib/pdfParser";
 import { createChunks } from "../../../lib/chunkText";
+import { createEmbedding } from "../../../lib/embeddings";
+import {saveEmbeddings} from "../../../lib/vectorStore"
+
 
 export async function POST(request) {
   try {
@@ -16,27 +19,53 @@ export async function POST(request) {
 
    const extractedText = await extractPdfText(pdfFile);
 
-console.log("========== PARSER DEBUG ==========");
-console.log("Characters:", extractedText.length);
-console.log(extractedText.slice(0, 300));
-console.log("=================================");
 
 const chunks = createChunks(extractedText);
 
-console.log("========== CHUNK DEBUG ==========");
-console.log("Characters:", extractedText.length);
-console.log("Words:", extractedText.replace(/\s+/g, " ").trim().split(" ").length);
-console.log("Chunks:", chunks.length);
-console.log("First Chunk Preview:");
-console.log(chunks[0]?.slice(0, 150));
-console.log("=================================");
+// console.log("========== CHUNK DEBUG ==========");
+// console.log("Characters:", extractedText.length);
+// console.log("Words:", extractedText.replace(/\s+/g, " ").trim().split(" ").length);
+// console.log("Chunks:", chunks.length);
+// console.log("First Chunk Preview:");
+// console.log(chunks[0]?.slice(0, 150));
+// console.log("=================================");
 
-return NextResponse.json({
-  message: "PDF parsed successfully!",
-  characters: extractedText.length,
-  totalChunks: chunks.length,
-  firstChunk: chunks[0],
-});
+  const vectors = [];
+
+  for(let i =0;i<chunks.length;i++){
+
+    console.log(`Generating embedding for Chunk ${i+1}....`);
+
+    const embedding = await createEmbedding(chunks[i]);
+
+    vectors.push({
+      id : i+1,
+      text : chunks[i],
+      embedding
+    })
+    
+    console.log(`Chunk ${i+1} Done ✅`);
+    
+  }
+
+  const filePath = await saveEmbeddings(vectors);
+
+  console.log("Saved Vector Database");
+  console.log(filePath);
+  
+  
+
+  
+  console.log("Embedding Length:", vectors[0].embedding.length);
+  console.log("First 10 Values:");
+  console.log(vectors[0].embedding.slice(0,10));
+
+  return NextResponse.json({
+    message: "Embeddings created successfully!",
+    totalChunks: chunks.length,
+    totalEmbeddings: vectors.length,
+    sampleEmbeddingLength: vectors[0].embedding.length,
+  });
 
   } catch (error) {
     console.error("UPLOAD ERROR:", error);
